bdd1803a84ac18db9d51004554815a4869a0c27a
Fred <frederico.a.soares@gmail.com>
2019-05-26 12:18:29 -0300

    Added react-bootstrap lib.

---
+++ src/components/login/LoginComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function Login() {
+
+    return (
+        <Button variant="primary">Primary</Button>
+    );
+}
+
+export default Login;
